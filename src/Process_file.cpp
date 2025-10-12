1bef74fc273891b16b49ecb7b835232a9d0d214b
Vanya Semenov <vanya-semenov_2002@list.ru>
2025-10-12 12:57:03 +0300

    Files update

---
+++ src/Process_file.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include "types.h"
+
+#define HEADER "Almost SQLite1"
+#define PAGE_SIZE 4096
+#define IS_BTREE 0
+#define UNICODE 1 //1 = UTF-8; 2 = UTF-16; 3 = UTF-32
+#define ENGINE_VERSION 1
+#define METADATA_SIZE 50
+
+using namespace std;
+
+void make_file(char* filepath, char read, char write) {
+	ofstream file(filepath, ios::binary);
+	if (file) {
+		file.write(HEADER, strlen(HEADER)); //14 bytes
+		int page_size = PAGE_SIZE;
+		char is_btree = IS_BTREE;
+		char unicode = UNICODE;
+		char engine = ENGINE_VERSION;
+		file.write(reinterpret_cast<const char*>(&page_size), sizeof(page_size)); //4 bytes
+		file.write(&is_btree, sizeof(char)); //1 byte
+		file.write(&unicode, sizeof(char)); //1 byte
+		file.write(&engine, sizeof(char)); //1 byte
+		file.write(&read, sizeof(char)); //1 byte
+		file.write(&write, sizeof(char)); //1 byte
+
+		char null = 0;
+		for (int i = 0; i < 27; i++) file.write(&null, sizeof(char)); //27 reserve bytes
+		// Metadata of binary file is 50 bytes
+		file.close();
+	}
+	else cout << "Error! File is not open!";
+}
+
+void make_page_meta(char* filepath, All_types* colomns) {
+	ofstream file(filepath, ios::binary, ios::in | ios::out);
+	file.seekp(METADATA_SIZE, ios::beg);
+
+}
+
+//void get_note()
+
+int do_meta() {
+	char filepath[256] = "C:\\Users\\vanya\\OneDrive\\Desktop\\almost_sqlite\\src\\file.bin";
+	cout << "Input the filepath";
+	make_file(filepath, 1, 1);
+
+
+}
---
+++ src/test_types.cpp
@@ -0,0 +1,153 @@
+#include "types.h"
+#include<string>
+#include <exception>
+#include <cmath>
+#include <sstream>
+
+class ErrorType : public std::exception {
+private:
+	std::string message;
+public:
+	explicit ErrorType(const std::string& msg) : message(msg) {}
+	const char* what() const noexcept override {
+		return message.c_str();
+	}
+};
+
+bool test_notNull(char* symb) {
+	if not(symb) return 0;
+}
+
+bool test_for_bit(char* bit, bool not_null) {
+	if (bit == NULL && not_null) return true;
+	if (bit == NULL && !not_null) return false;
+	if (bit != '0' && bit != '1') return false;
+	else return true;
+}
+
+bool test_for_tinyint(char* tinyint, bool not_null) {
+	if (tinyint == NULL && not_null) return true;
+	if (tinyint == NULL && !not_null) return false;
+	if (strlen(tinyint) > 3) return 0;
+	for (di : tinyint) {
+		if (!isdigit(di))
+			return false;
+	}
+	if (strcmp("255", tinyint) && strlen(tinyint) == 3) return 1;
+	else if (strlen(tinyint) < 3) return 1;
+	else return 0;
+}
+
+bool test_for_smallint(char* smallint, bool not_null) {
+	if (smallint == NULL && not_null) return true;
+	if (smallint == NULL && !not_null) return false;
+	if (smallint[0] == '-') {
+		if (strlen(smallint) > 6) return false;
+		smallint++;
+		for (di : smallint) {
+			if (!isdigit(di))
+				return false;
+		}
+		if (strcmp("32768", smallint) >= 0 && strlen(smallint) == 5)return true;
+		else if (strlen(smallint) < 5) return true;
+		else return false;
+	}
+
+	if (smallint[0] != '-') {
+		if (strlen(smallint) > 5) return false;
+		for (di : smallint) {
+			if (!isdigit(di))
+				return false;
+		}
+		else if (strcmp("32767", smallint) >= 0 && strlen(smallint) == 5) return true;
+		else if (strlen(smallint) < 5) return true;
+		else return false;
+	}
+}
+
+bool test_for_int(char* integer, bool not_null) {
+	if (integer == NULL && not_null) return true;
+	if (integer == NULL && !not_null) return false;
+	if (integer[0] == '-') {
+		if (strlen(integer) > 11) return false;
+		integer++;
+		for (di : integer) {
+			if (!isdigit(di))
+				return false;
+		}
+		if (strcmp("2147483648", integer) >= 0 && strlen(integer) == 10)return true;
+		else if (strlen(integer) < 10) return true;
+		else return false;
+	}
+
+	if (integer[0] != '-') {
+		for (di : integer) {
+			if (!isdigit(di))
+				return false;
+		}
+		if (strlen(integer) > 10) return false;
+		else if (strcmp("2147483647", integer) >= 0 && strlen(integer) == 10) return true;
+		else if (strlen(integer) < 10) return true;
+		else return false;
+	}
+}
+
+
+bool test_for_bigint(char* bigint, bool not_null) {
+	if (bigint == NULL && not_null) return true;
+	if (bigint == NULL && !not_null) return false;
+	if (bigint[0] == '-') {
+		if (strlen(bigint) > 19) return false;
+		bigint++;
+		for (di : bigint) {
+			if (!isdigit(di))
+				return false;
+		}
+		if (strcmp("9223372036854775808", bigint) >= 0 && strlen(bigint) == 19)return true;
+		else if (strlen(bigint) < 19) return true;
+		else return false;
+	}
+
+	if (bigint[0] != '-') {
+		for (di : bigint) {
+			if (!isdigit(di))
+				return false;
+		}
+		if (strlen(bigint) > 19) return false;
+		else if (strcmp("9223372036854775807", bigint) >= 0 && strlen(integer) == 19) return true;
+		else if (strlen(bigint) < 19) return true;
+		else return false;
+	}
+}
+
+bool test_for_float(char* float_type, bool not_null) {
+	if (float_type == NULL && not_null) return true;
+	if (float_type == NULL && !not_null) return false;
+	for (di : float_type) {
+		if (!(isdigit(di) || di == '.' || di == '-'))
+			return false;
+	}
+	std::stringstream ss(float_type);
+	double value;
+	if (!(ss >> value)) {
+		trow ErrorType("Ќе удовлетвор€ет требовани€ми типа данных");
+		return false;
+	}
+	return true;
+}
+
+bool test_for_real(char* real, bool not_null) {
+	if (real == NULL && not_null) return true;
+	if (real == NULL && !not_null) return false;
+	for (di : real) {
+		if (!(isdigit(di) || di == '.' || di == '-'))
+			return false;
+	}
+	std::stringstream ss(real);
+	float value;
+	if (!(ss >> value)) {
+		trow ErrorType("Ќе удовлетвор€ет требовани€ми типа данных");
+		return false;
+	}
+	return true;
+}
